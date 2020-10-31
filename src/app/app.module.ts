b172b0b6c916219e536462e65f5daeaac439840f
Yi Long <ongyl88@gmail.com>
2020-10-31 15:32:38 +0800

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InvestmentComponent } from './investment/investment.component';
+import { InsuranceComponent } from './insurance/insurance.component';
+import { LiabilitiesComponent } from './liabilities/liabilities.component';
+import { SavingsComponent } from './savings/savings.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    InvestmentComponent,
+    InsuranceComponent,
+    LiabilitiesComponent,
+    SavingsComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
