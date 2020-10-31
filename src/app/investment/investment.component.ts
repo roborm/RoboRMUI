1f971e7eee87cdafc3f4078d83d79e93a0f2c534
Yi Long <ongyl88@gmail.com>
2020-10-31 17:22:17 +0800

    initial

--- src/app/investment/investment.component.ts
+++ src/app/investment/investment.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-investment',
   templateUrl: './investment.component.html',
-  styleUrls: ['./investment.component.css']
+  styleUrls: ['./investment.component.css'] 
 })
 export class InvestmentComponent implements OnInit {
 
