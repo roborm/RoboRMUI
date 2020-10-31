7ee936909f9a5c2b435f48ac0f5db3f854b2c50c
Yi Long <ongyl88@gmail.com>
2020-10-31 15:53:39 +0800

    test

--- src/app/investment/investment.component.ts
+++ src/app/investment/investment.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvestmentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
   }
